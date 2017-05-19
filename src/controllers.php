93b56724f1ac083114cc7798863ba4d34e300808
Your Name <you@example.com>
2017-05-19 17:57:50 +0200

    Ajout d'une route de test

--- src/controllers.php
+++ src/controllers.php
@@ -14,6 +14,10 @@ $app->get('/', function () use ($app) {
 ->bind('homepage')
 ;
 
+$app->get('/test', function(){
+    return 'Hello test (Emeline) !!!';
+});
+
 $app->error(function (\Exception $e, Request $request, $code) use ($app) {
     if ($app['debug']) {
         return;
